0e4258c90cf43041671c92ace16d30ee7309da12
Abraham Obianke <cruzcontactdesk@gmail.com>
2025-02-02 19:58:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,80 +1,70 @@
 import joblib
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, request, jsonify
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import InputRequired, Email, Length
-from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
-# from flask_login import LoginManpercentage_of_time_with_abnormal_long_term_variabilityr, UserMixin, login_user, login_required, logout_user
-import pandas as pd
+from wtforms import FloatField, SubmitField
+from wtforms.validators import InputRequired, NumberRange
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.neighbors import KNeighborsClassifier
+from fhir.resources.diagnosticreport import DiagnosticReport
+from fhir.resources.observation import Observation
 
-
-
-
+# Load ML model
 filename1 = 'model/fetal_health_risk.pkl'
 model1 = joblib.load(filename1)
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+Bootstrap(app)
 
-
-app = Flask(__name__, template_folder='templates')
-
-
-##################################################################################
+# Define FlaskForm
+class FetalHealthForm(FlaskForm):
+    percentage_of_time_with_abnormal_long_term_variability = FloatField("Percentage of Abnormal Long-Term Variability", validators=[InputRequired()])
+    abnormal_short_term_variability = FloatField("Abnormal Short-Term Variability", validators=[InputRequired()])
+    mean_value_of_short_term_variability = FloatField("Mean Value of Short-Term Variability", validators=[InputRequired()])
+    accelerations = FloatField("Accelerations", validators=[InputRequired()])
+    prolongued_decelerations = FloatField("Prolongued Decelerations", validators=[InputRequired()])
+    histogram_mode = FloatField("Histogram Mode", validators=[InputRequired()])
+    uterine_contractions = FloatField("Uterine Contractions", validators=[InputRequired()])
+    histogram_median = FloatField("Histogram Median", validators=[InputRequired()])
+    histogram_mean = FloatField("Histogram Mean", validators=[InputRequired()])
     
-@app.route('/')
+    submit = SubmitField("Predict")
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("fetal.html")
+    form = FetalHealthForm()
+    prediction_text = None
 
+    if form.validate_on_submit():
+        # Collect form data
+        data = np.array([[
+            form.percentage_of_time_with_abnormal_long_term_variability.data,
+            form.abnormal_short_term_variability.data,
+            form.mean_value_of_short_term_variability.data,
+            form.accelerations.data,
+            form.prolongued_decelerations.data,
+            form.histogram_mode.data,
+            form.uterine_contractions.data,
+            form.histogram_median.data,
+            form.histogram_mean.data
+        ]])
 
-@app.route("/fetal")
-def fetal():
-    return render_template("fetal.html")
-
-##################################################################################
-
-@app.route('/predictfetal', methods=['POST'])
-def predictfetal():
-    if request.method == 'POST':
-        # Get form inputs
-        percentage_of_time_with_abnormal_long_term_variability = float(request.form['percentage_of_time_with_abnormal_long_term_variability'])
-        abnormal_short_term_variability = float(request.form['abnormal_short_term_variability'])
-        mean_value_of_short_term_variability = float(request.form['mean_value_of_short_term_variability'])
-        accelerations = float(request.form['accelerations'])
-        prolongued_decelerations = float(request.form['prolongued_decelerations'])
-        histogram_mode = float(request.form['histogram_mode'])
-        uterine_contractions = float(request.form['uterine_contractions'])
-        histogram_median = float(request.form['histogram_median'])
-        histogram_mean = float(request.form['histogram_mean'])
-       
-              
-
-        # Create a NumPy array with the input data
-        data = np.array([[percentage_of_time_with_abnormal_long_term_variability, 
-                          abnormal_short_term_variability, mean_value_of_short_term_variability, 
-                          accelerations, prolongued_decelerations, 
-                          histogram_mode, uterine_contractions, histogram_median, 
-                          histogram_mean]])
-        
         # Make prediction
-        my_prediction = model1.predict(data)
-        
-        # Convert prediction to text
-        prediction_text = ""
-        if  my_prediction == 3:
-            prediction_text = "Patient Health is Pathological"
-        elif my_prediction == 2:
-            prediction_text = "Patient Health is Suspicious"
-        else:
-            prediction_text = "Patient Health is Normal"
+        my_prediction = model1.predict(data)[0]
 
+        # Convert prediction to readable text
+        prediction_text = "Pathological" if my_prediction == 3 else "Suspicious" if my_prediction == 2 else "Normal"
 
-        return render_template('fetal_result.html', prediction_text=prediction_text)
+        # Convert to FHIR JSON
+        diagnostic_report = DiagnosticReport.construct(
+            status="final",
+            code={"coding": [{"system": "http://loinc.org", "code": "57067-1", "display": "Fetal Health Assessment"}]},
+            conclusion=prediction_text
+        )
 
-##################################################################################
+        return render_template('fetal_result.html', prediction_text=prediction_text, fhir_output=diagnostic_report.json())
+
+    return render_template("fetal.html", form=form, prediction_text=prediction_text)
 
 if __name__ == "__main__":
     app.run(debug=True)
